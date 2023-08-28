1b59e7eeae82860f82ecdd6698f76a56706d7c96
WearyWasTaken <barneydestroyer2@gmail.com>
2023-08-28 15:57:41 +0800

    Added Descriptions to posts

--- resume-website/src/Post.js
+++ resume-website/src/Post.js
@@ -1,14 +1,15 @@
 export default function Post({head, body}){
 
     return(
-        <div class = "container mx-auto text-black box-border w-1/2 divide-y-4 bg-white">
+        <div class = "rounded-lg mx-auto text-white w-2/3 backdrop-blur bg-white/25">
+            
             <div 
             class = "flex justify-start text-5xl capitalize p-5"
             >
                 <p>{head}</p>
             </div>
             <div 
-            className = "flex justify-start post-body p-5"
+            class = "flex justify-start text-left text-xl p-5"
             >
                 <p>{body}</p>
             </div>
